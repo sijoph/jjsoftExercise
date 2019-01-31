cb67e452195c9d5a21905e2db73eb62337ea3245
sijoph <sijoph@users.noreply.github.com>
2019-01-31 11:34:12 +0530

    Header File
    Header file with bootstrap library links.

---
+++ header.php
@@ -0,0 +1,22 @@
+   
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Comparative Market Analysis </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  
+</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      
+    </div>
+    
+  </div>
+</nav>
+<div class="container"> 
