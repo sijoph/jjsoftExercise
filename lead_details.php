593c5978b36c8437c5f8aa7de0298f8f03a49a5d
sijoph <sijoph@users.noreply.github.com>
2019-01-31 12:21:01 +0530

    Final Commit
    Single Lead Details Final Commit

--- lead_details.php
+++ lead_details.php
@@ -1,11 +1,11 @@
 <?php 
 include('header.php'); /*include header*/
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 include_once "class.php";
 $lead = new lead; /*lead object creation*/
-
+ 
 ?>
-	
 	<h2>Lead Details</h2>
  		<div class="col-md-10">
 			<table class="table table-bordered">
@@ -24,10 +24,10 @@ $lead = new lead; /*lead object creation*/
 					<?php 
 					if(isset($_GET['luid']))
 					{
-						$lead->single_lead_details($_GET['luid']);
+						$lead->single_lead_details($_GET['luid']); /*single lead details with row*/
 					} else {
 						
-						echo "invalid";
+						echo "<tr><td colspan='6'>Invalid Request !</td></tr>";
 					}
 						
 					?>
