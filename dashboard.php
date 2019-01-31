f731b9d156dff7c1c52cf9183aa00d1e6e411ee2
sijoph <sijoph@users.noreply.github.com>
2019-01-31 12:03:33 +0530

    auid impemenation
    Agent dashboard only will get by Correct auid

--- dashboard.php
+++ dashboard.php
@@ -5,7 +5,11 @@ ini_set('display_errors', 1);
 include_once "class.php";
 $lead = new lead; /*lead object creation*/
 
- 
+ if(isset($_GET['auid']))
+ {
+	
+	if($_GET['auid']=='603AEC402B9363218DED7762BA6A3D3E')
+	{
 ?>
 	<h2>Agent Dashboard : All Leads</h2>
  		<div class="col-md-6">
@@ -29,6 +33,21 @@ $lead = new lead; /*lead object creation*/
 	 
 	
 <?php 
- 
+	}
+	else 
+	{
+		?>
+		<div class='alert alert-danger'>You Dont Have Permission To Access This Page !</div>
+		<?php
+		
+	}
+}
+else 
+{
+	?>
+	<div class='alert alert-danger'>You Dont Have Permission To Access This Page !</div>
+	<?php
+	
+}
  include('footer.php');	
 ?>
