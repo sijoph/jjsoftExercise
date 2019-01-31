2a08e99f0be169cec8e9aa77f1ed4fbdac97df94
sijoph <sijoph@users.noreply.github.com>
2019-01-31 11:43:22 +0530

    Ajax Request Response
    Show Ajax Request Response.

--- lead_submit.php
+++ lead_submit.php
@@ -6,10 +6,10 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
  	$created_date = date('Y-m-d H:i:s');
 	$lead_insert = $lead->lead_insert($_REQUEST['uniqid'], $_REQUEST['fname'], $_REQUEST['lname'], $_REQUEST['email'], $_REQUEST['phone'], $_REQUEST['address'], $_REQUEST['squarefoot'], $created_date);
 	if($lead_insert){
-		echo "success";
+		echo "<div class='alert alert-success'>Information Has Been Saved</div>";
 	} else {
 		
-		echo "failed";
+		echo "<div class='alert alert-danger'>Lead Submission Failed !</div>";
 	}
 	
 }
